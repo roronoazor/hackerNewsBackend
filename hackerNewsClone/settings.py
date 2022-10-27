5458f347d2e3e55b49b0f4610f5d13b89063c36e
roronoazor <ugoodumegwu@gmail.com>
2022-10-27 17:52:50 +0100

    fix: updated settings.py

--- hackerNewsClone/settings.py
+++ hackerNewsClone/settings.py
@@ -84,6 +84,7 @@ WSGI_APPLICATION = 'hackerNewsClone.wsgi.application'
 
 
 DATABASES = dict()
+DATABASES['default'] = dict()
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
