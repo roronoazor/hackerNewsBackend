b13b0ff9231572b47e26706d4d0e07464b75f59e
roronoazor <ugoodumegwu@gmail.com>
2022-10-28 05:57:35 +0100

    chore: updated requirements.txt

--- core/queries/get_items.py
+++ core/queries/get_items.py
@@ -12,11 +12,11 @@ def get_items_query(request):
     filters = dict()
     
     if request.query_params.get(PARAM_QUERY_BY_TYPE):
-        filters['type'] = request.query_params.get(PARAM_QUERY_BY_TYPE)
+        filters['type'] = request.query_params.get(PARAM_QUERY_BY_TYPE, '')
         
     if request.query_params.get(PARAM_QUERY_BY_TEXT):
-        filters['text__icontains'] = request.query_params.get(PARAM_QUERY_BY_TEXT)
-        filters['title__icontains'] = request.query_params.get(PARAM_QUERY_BY_TEXT)
+        filters['text__icontains'] = request.query_params.get(PARAM_QUERY_BY_TEXT, '')
+        filters['title__icontains'] = request.query_params.get(PARAM_QUERY_BY_TEXT, '')
         
     return Item.objects.filter( 
                                Q(type=request.query_params.get('type')) |
