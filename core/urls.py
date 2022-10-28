af7ee7ec069eb0ac97b23a936cff63f1dc65c0d2
roronoazor <ugoodumegwu@gmail.com>
2022-10-28 11:57:19 +0100

    feat: add delete functionality

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
-from .views import GetCreateItemView
+from .views import GetCreateItemView, DeleteItemView
 from django.urls import path
 
 
 urlpatterns = [
-    path('items/', GetCreateItemView.as_view(), name='items')
+    path('items/', GetCreateItemView.as_view(), name='items'),
+    path('items/delete/', DeleteItemView.as_view(), name='delete')
 ]
